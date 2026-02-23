6568f86b261c9305921d38f5eeee3922759171c3
Sam Harder <samuel.a.harder@gmail.com>
2026-02-22 20:02:10 -0600

    Basic skeleton of package
    I'm using vite to have hot module reloading on the test page I'll
    use for building the rubiks cube app. Followed the vite
    docs for setting up the package.json and config.

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'RubiksCube',
+      fileName: 'index',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['three'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          three: 'THREE',
+        },
+      },
+    },
+  },
+})
